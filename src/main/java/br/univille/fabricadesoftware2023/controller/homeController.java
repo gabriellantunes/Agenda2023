f9db7c9b3f814063708543933c71139d60dfdd0a
gabriellantunes <gabrielantunesbernardiu@gmail.com>
2023-04-03 21:37:42 -0300

    atualizacao

--- src/main/java/br/univille/fabricadesoftware2023/controller/homeController.java
+++ src/main/java/br/univille/fabricadesoftware2023/controller/homeController.java
@@ -1,5 +1,8 @@
 package br.univille.fabricadesoftware2023.controller;
 
+import java.util.HashMap;
+
+import javax.el.MapELResolver;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +16,9 @@ public class homeController {
     @GetMapping
     @ResponseBody
     public ModelAndView index() {
-        return new ModelAndView("home/index");
+        HashMap<String, String> map = new HashMap<>();
+        map.put("apelido", "valor");
+        map.put("nome","zezinho");
+        return new ModelAndView("home/index",map);
     }
 }
